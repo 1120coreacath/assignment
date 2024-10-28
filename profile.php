0f8e9ec50d48fdb8edfb677df87f439e3fde9014
coreacathrine <146171629+1120coreacath@users.noreply.github.com>
2024-10-28 13:14:56 +0800

    profile.php

---
+++ profile.php
@@ -0,0 +1,12 @@
+<?php
+$user = [
+    "username" => "JohnDoe",
+    "email" => "john@example.com",
+    "age" => 30
+];
+
+echo "User Profile:<br>";
+echo "Username: " . $user["username"] . "<br>";
+echo "Email: " . $user["email"] . "<br>";
+echo "Age: " . $user["age"];
+?>
